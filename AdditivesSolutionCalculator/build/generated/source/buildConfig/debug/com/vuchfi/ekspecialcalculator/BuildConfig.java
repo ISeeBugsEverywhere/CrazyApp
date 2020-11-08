9ac7977f524e02b53a370a36d38ed170ef53c1d9
bugsStillEverywhere <menulio.svytuokle@gmail.com>
2020-11-08 09:02:35 +0200

    refactoring

--- AdditivesSolutionCalculator/build/generated/source/buildConfig/debug/com/vuchfi/ekspecialcalculator/BuildConfig.java
+++ AdditivesSolutionCalculator/build/generated/source/buildConfig/debug/com/vuchfi/ekspecialcalculator/BuildConfig.java
@@ -5,7 +5,7 @@ package com.vuchfi.ekspecialcalculator;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.vuchfi.crazyapp";
+  public static final String APPLICATION_ID = "com.vuchfi.eksimplecalculator";
   public static final String BUILD_TYPE = "debug";
   public static final int VERSION_CODE = 1;
   public static final String VERSION_NAME = "1.0";
