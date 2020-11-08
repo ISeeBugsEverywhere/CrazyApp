7b6a7109de8cf932636ecd4fdcf1485f5718c359
bugsStillEverywhere <menulio.svytuokle@gmail.com>
2020-11-08 08:49:05 +0200

    additives

--- app/src/main/java/com/vuchfi/crazyapp/ui/solution/SolutionFragment.java
+++ app/src/main/java/com/vuchfi/crazyapp/ui/solution/SolutionFragment.java
@@ -38,6 +38,8 @@ public class SolutionFragment extends Fragment {
     double LiTFSI = 0.275;
     double FK209 = 0.12;
 
+    double solutionVolume = -1.0;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -104,6 +106,7 @@ public class SolutionFragment extends Fragment {
             double liters = (pmass*mG)/mgml;
             double milliliters = Math.round((liters/1e-3)*1e4)/1e4;
             double microliters = milliliters*1000.0;
+            solutionVolume = milliliters;
             Snackbar.make(getView(), String.valueOf(milliliters)+" ml,\narba "+String.valueOf(microliters)+" μl", 3000).show();
         }
         catch (Exception ex)
@@ -115,41 +118,30 @@ public class SolutionFragment extends Fragment {
     private void additivesFunction()
     {
 
-        if (mMButton.isChecked()){
-            mM = 1e-3;
-        }
-        else if (MButton.isChecked())
+        if (solutionVolume <= 0.0)
         {
-            mM = 1.0;
+            solutionFunction();
+            showAdditives();
         }
-        if (mgButton.isChecked())
+        else
         {
-            mG = 1e-3;
+            showAdditives();
         }
-        else if (gButton.isChecked())
-        {
-            mG = 1.0;
-        }
-        try {
-            mmass = Double.parseDouble(molarMassEntry.getText().toString());
-            pmass = Double.parseDouble(pickedMassEntry.getText().toString());
-            molc = Double.parseDouble(molarsEntry.getText().toString());
-            double mgml = mM*molc*mmass;
-            double liters = (pmass*mG)/mgml;
-            double milliliters = Math.round((liters/1e-3)*1e4)/1e4;
-            double div = 1/(milliliters);
-            double vtBP = Math.round((tBP/div)*100000)/100000;
-            double vLiTFSI = Math.round((LiTFSI/div)*100000)/100000;
-            double vFK209 = Math.round((FK209/div)*100000)/100000;
-            String TBP = "tBP : "+String.valueOf(vtBP)+" ml";
-            String LITFSI = "LiTFSI (520 mg/ml) : "+String.valueOf(vLiTFSI)+" ml";
-            String FK = "FK209 (320 mg/ml) : "+ String.valueOf(vFK209)+" ml";
-            Snackbar.make(getView(), TBP+"\n"+LITFSI+"\n"+FK, 5000).show();
-        }
-        catch (Exception exception)
-        {
-            Toast.makeText(getActivity(), "Problemos", Toast.LENGTH_SHORT).show();
-        }
+
+    }
+
+    private void showAdditives()
+    {
+        LiTFSI = Math.round((solutionVolume * 0.275)*10000.0)/10000.0;
+        tBP = Math.round((solutionVolume * 0.485)*10000.0)/10000.0;
+        FK209 = Math.round((solutionVolume * 0.12)*10000.0)/10000.0;
+        String answer = "tBP - "+String.valueOf(tBP)+" ml\nLiTFSI - "+String.valueOf(LiTFSI)+" ml\nFK209 - "+String.valueOf(FK209)+" ml";
+        Snackbar snackbar = Snackbar.make(getView(), answer, 5000);
+        View sbView = snackbar.getView();
+        TextView textView =  sbView.findViewById(R.id.snackbar_text);
+        // For multi-line text, limit max line count.
+        textView.setMaxLines(3);
+        snackbar.show();
 
     }
 }
